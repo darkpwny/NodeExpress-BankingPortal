f6963be5091e3b6530df2f966678196f124e52b0
rick.donaldson <rick.donaldson@morrisonds.com>
2023-03-15 21:33:02 +0000

    module 2

--- src/app.js
+++ src/app.js
@@ -5,11 +5,40 @@ const express = require('express');
 const app = express();
 
 const viewpath = path.join( __dirname,'views');
-const indexViewBag = {title: 'index'};
+
 
 app.set('views',viewpath);
 app.set('view engine','ejs');
 app.use(express.static('src/public'));
 
+const accountData = fs.readFileSync('src/json/accounts.json','utf8');
+const account = JSON.parse(accountData);
+console.log(account);
+
+const userData = fs.readFileSync('src/json/users.json','utf8');
+const user = JSON.parse(userData);
+console.log(user);
+
+const indexViewBag = {
+    title: 'Account Summary',
+    accounts: account
+    };
+
+const savingsViewBag = {
+    title: 'Savings',
+    account: account.savings
+    };
+const checkingViewBag = {
+    title: 'Checking',
+    account: account.savings
+    };
+const creditViewBag = {
+    title: 'Credit',
+    account: account.savings
+    };
+
 app.get('/',(req,res)=>{res.render('index', indexViewBag);});
+app.get('/savings',(req,res)=>{res.render('account', savingsViewBag);});
+app.get('/credit',(req,res)=>{res.render('account', creditViewBag);});
+app.get('/checking',(req,res)=>{res.render('account', checkingViewBag);});
 app.listen(3000,()=>console.log('PS Project Running on port 3000!'));
