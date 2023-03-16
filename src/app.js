efcfac205d6a6f01e3006aac7f12d64e40f90fe9
rick.donaldson <rick.donaldson@morrisonds.com>
2023-03-16 14:43:49 +0000

    module 3

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const { response } = require('express');
 const app = express();
 
 const viewpath = path.join( __dirname,'views');
 
 app.set('views',viewpath);
 app.set('view engine','ejs');
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('src/public'));
 
 const accountData = fs.readFileSync('src/json/accounts.json','utf8');
@@ -37,4 +39,23 @@ app.get('/savings',(req,res)=>{res.render('account', savingsViewBag);});
 app.get('/credit',(req,res)=>{res.render('account', creditViewBag);});
 app.get('/checking',(req,res)=>{res.render('account', checkingViewBag);});
 app.get('/profile',(req,res)=>{res.render('profile', profileViewBag);});
+
+app.get('/transfer',(req,res)=>{res.render('transfer');});
+app.post('/transfer',(req,res)=>{
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+    accounts[req.body.to].balance =   parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
+    res.render('transfer',{message: "Transfer Completed"})
+});
+
+app.get('/payment',(req,res)=>{res.render('payment',{account: accounts.credit});});
+app.post('/payment',(req,res)=>{
+    accounts.credit.balance = accounts.credit.balance - req.body.amount;
+    accounts.credit.available = parseInt(accounts.credit.available)+parseInt(req.body.amount);
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
+    res.render('payment',{message: "Payment Successful", account: accounts.credit});
+});
+
 app.listen(3000,()=>console.log('PS Project Running on port 3000!'));
