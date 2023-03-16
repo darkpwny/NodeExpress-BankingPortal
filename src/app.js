be5c137631009b5364c7732343f2585c5dc08c98
Rick Donaldson <rick.donaldson@morrisonds.com>
2023-03-16 09:41:30 +0000

    Revert "module 2"
    This reverts commit f6963be5091e3b6530df2f966678196f124e52b0.

--- src/app.js
+++ src/app.js
@@ -5,43 +5,11 @@ const express = require('express');
 const app = express();
 
 const viewpath = path.join( __dirname,'views');
-
+const indexViewBag = {title: 'index'};
 
 app.set('views',viewpath);
 app.set('view engine','ejs');
 app.use(express.static('src/public'));
 
-const accountData = fs.readFileSync('src/json/accounts.json','utf8');
-const accounts = JSON.parse(accountData);
-console.log(accounts);
-
-const userData = fs.readFileSync('src/json/users.json','utf8');
-const users = JSON.parse(userData);
-console.log(users);
-
-const indexViewBag = {
-    title: 'Account Summary',
-    accounts: accounts
-    };
-
-const savingsViewBag = {
-    account: accounts.savings
-    };
-const checkingViewBag = {
-    account: accounts.checking
-    };
-const creditViewBag = {
-    account: accounts.credit
-    };
-    
-const profileViewBag =
-{
-    user : users[0]
-};
-
 app.get('/',(req,res)=>{res.render('index', indexViewBag);});
-app.get('/savings',(req,res)=>{res.render('account', savingsViewBag);});
-app.get('/credit',(req,res)=>{res.render('account', creditViewBag);});
-app.get('/checking',(req,res)=>{res.render('account', checkingViewBag);});
-app.get('/profile',(req,res)=>{res.render('profile', profileViewBag);});
 app.listen(3000,()=>console.log('PS Project Running on port 3000!'));
