3c0dd0bda5da60b6d956fbfc72a69b06c33a0bb7
rick.donaldson <rick.donaldson@morrisonds.com>
2023-03-17 09:08:08 +0000

    module 4

--- src/app.js
+++ src/app.js
@@ -11,10 +11,9 @@ app.set('view engine','ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('src/public'));
 
-const accountData = fs.readFileSync('src/json/accounts.json','utf8');
-const accounts = JSON.parse(accountData);
-const userData = fs.readFileSync('src/json/users.json','utf8');
-const users = JSON.parse(userData);
+const {accounts} = require ('./data');
+const {users} = require ('./data');
+const {writeJSON} = require ('./data');
 
 const indexViewBag = {
     title: 'Account Summary',
@@ -44,8 +43,7 @@ app.get('/transfer',(req,res)=>{res.render('transfer');});
 app.post('/transfer',(req,res)=>{
     accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
     accounts[req.body.to].balance =   parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
-    const accountsJSON = JSON.stringify(accounts);
-    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
+    writeJSON();
     res.render('transfer',{message: "Transfer Completed"})
 });
 
@@ -53,8 +51,7 @@ app.get('/payment',(req,res)=>{res.render('payment',{account: accounts.credit});
 app.post('/payment',(req,res)=>{
     accounts.credit.balance = accounts.credit.balance - req.body.amount;
     accounts.credit.available = parseInt(accounts.credit.available)+parseInt(req.body.amount);
-    const accountsJSON = JSON.stringify(accounts);
-    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
+    writeJSON();
     res.render('payment',{message: "Payment Successful", account: accounts.credit});
 });
 
